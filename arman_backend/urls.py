943637766adc934c53fc529209ecfb61e5f47e45
Rojin4455 <rojinsaji35@gmail.com>
2025-05-28 18:20:46 +0530

    added refresh token logic

--- arman_backend/urls.py
+++ arman_backend/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include("accounts.urls")),
     path('api/data/', include("data_management_app.urls")),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
